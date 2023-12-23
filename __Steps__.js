173d71a369868ea5701d9e6dcc9d96703c0fffdb
shahin hossain <shahinhossain3@hotmail.com>
2023-12-23 17:16:42 +0600

    create basic server

---
+++ __Steps__.js
@@ -0,0 +1,13 @@
+/**
+ * Server init
+ * npm i express cors dotenv
+ *run express server 
+ *
+ */
+
+/**
+ * Client Server
+ * create a project with react router
+ * install tailwind 
+ * install daisy UI
+ */
